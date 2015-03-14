b0b324f8d96d4b22d2060b934b09a03666856439
francoisrd <akdave@mac.com>
2015-03-13 21:11:59 -0700

    more rr updates

--- reresearch/processactivitydata.R
+++ reresearch/processactivitydata.R
@@ -5,6 +5,7 @@
 library(plyr)
 library(dplyr)
 library(lubridate)
+library(ggplot2)
 
 #load data to a data frame
 dfraw <- read.csv("./activity.csv")
@@ -15,6 +16,7 @@ rm(dfraw)
 
 #mutate data as needed to make it easier to work with
 tdf1 <- transmute(tdfraw, date=ymd(date), interval, steps)
+
 createweekendfactor <- function(wdayints){
 	#create empty container for char vector
 	charout <- character(length(wdayints))
@@ -27,6 +29,7 @@ createweekendfactor <- function(wdayints){
 	
 	return(as.factor(charout))
 }
+
 tdf <- mutate(tdf1, 
 	dayofweek = wday(date), 
 	partofweek = createweekendfactor(dayofweek))
@@ -37,3 +40,18 @@ tdf_bydate <- group_by(tdf, date)
 tdf_byday <- group_by(tdf, dayofweek)
 tdf_bypart <- group_by(tdf, partofweek)
 tdf_byint <- group_by(tdf, interval)
+
+#create mean and median data from grouped data
+mms_bydate <- summarise(tdf_bydate,
+	meanstepsbydate=mean(steps, na.rm=T),
+	medstepbydate=median(steps, na.rm=T),
+	sumstepbydate=sum(steps, na.rm=T))
+	 
+mms_byint <- summarise(tdf_byint,
+	meanstepbyint=mean(steps, na.rm=T),
+	medstepbyint=median(steps, na.rm=T),
+	sumstepbyint=sum(steps, na.rm=T)) 
+	 
+histbydate <- ggplot(data=mm_bydate) + geom_histogram(aes(x=meanbydate))
+linebyint <- ggplot(data=mms_byint) + geom_line(aes(x=interval, y=meanstepbyint))
+
