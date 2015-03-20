11633301a8c468bce27c0d6b12e21b23a0349413
francoisrd <akdave@mac.com>
2015-03-20 11:26:21 -0700

    more stat inf project updates

--- statinf/simexpanalysis.R
+++ statinf/simexpanalysis.R
@@ -30,24 +30,28 @@ dfdatasim <- tbl_df(dfdatasim)
 #means of sim data
 dfmeansim <- tbl_df(data.frame(meansimexp=colMeans(msimdata)))
 
-#create sim data for theoretical distribution of means
-dfmeanth <- tbl_df(data.frame(meanth=rnorm(nmeansims,
-	 mean=thmeanmean, sd=thmeansd)))
-
 #calculate summary statistics of sim data and theoretical
+meanmeans <- mean(dfmeansim$meansimexp)
+medmeans <- median(dfmeansim$meansimexp)
+varmeans <- var(dfmeansim$meansimexp)
 
+#create sim data for theoretical distribution of means, what it should look like
+dfmeanth <- tbl_df(data.frame(meanth=rnorm(10*nmeansims,
+	 mean=thmeanmean, sd=thmeansd)))
 
 #Compare sim results to theoretical
 #hist of just simulated mean data
 simmeanhist <- ggplot(data=dfmeansim, aes(x=meansimexp)) 
 simmeanhist <- simmeanhist + geom_histogram(binwidth=0.1)
+simmeanhist <- simmeanhist + geom_vline(xintercept=thmeanmean, color='red')
 
 #hist of theoretical distribution
 thmeanhist <- ggplot(data=dfmeanth, aes(x=meanth)) 
 thmeanhist <- thmeanhist + geom_histogram(binwidth=0.1)
+thmeanhist <- thmeanhist + geom_vline(xintercept=thmeanmean, color='red')
 
-#hist of a individual sim cases
-exphist1 <- ggplot(data=dfdatasim, aes(x=sim1)) + geom_histogram(binwidth=3)
-exphist2 <- ggplot(data=dfdatasim, aes(x=sim2)) + geom_histogram(binwidth=3)
+#hist of an individual exponential
+dfsimexp <- tbl_df(data.frame(exp=rexp(nmeansims, lambda)))
+exphist1 <- ggplot(data=dfsimexp, aes(x=exp)) + geom_histogram(binwidth=2)
 
 
